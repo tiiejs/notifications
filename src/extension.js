628ba1cf1b185edbdd85add2e4c565a308e6ca70
Paweł Bobryk <bobryk.pawel@gmail.com>
2019-02-03 22:25:07 +0100

    [developing] Add requires of '@frames' fot notifications.

--- src/extension.js
+++ src/extension.js
@@ -4,6 +4,12 @@ import NotificationsService from "Tiie/Notifications/Service";
 import style from "./resources/style.scss";
 
 export default function(app, params = {}) {
+    if(!app.components().exists("@frames")) {
+        console.warn("I can not init notifications extension because there is no '@frames' service.");
+
+        return 0;
+    }
+
     // Init notifications service. Notifications need Frames service to work
     // properly.
     let service = new NotificationsService(app.components().get("@frames"));
